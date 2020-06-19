68fdb36b5163d7f43c9657bbbf252306776543cb
Robert Middleton <robert.middleton@rm5248.com>
2020-06-19 19:33:01 -0400

    can't do dynamic axes

--- vars/buildDebPkg.groovy
+++ vars/buildDebPkg.groovy
@@ -1,49 +1,33 @@
-def call( String[] distros = [], String[] arches = [] ){
+def call( String distro, String arch ){
 	pipeline {
 		agent { label 'master' }
 
 		stages {
-			stage('MainBuild'){
-				matrix {
-					axes {
-						axis {
-							name 'DISTRO'
-							values distros
+			stage('Clean'){
+				steps{
+					cleanWs()
+				}
+			}
+			stage('Checkout'){
+				steps{
+					fileOperations([folderCreateOperation('source')])
+					dir('source'){
+						checkout scm
+					}
+				}
+			}
+			stage("Build-${arch}-${distro}"){
+				steps{
+					if( distro == "jessie" ){
+					}else if( distro == "stretch" ){
+					}else if( distro == "buster" ){
+						configFileProvider([configFile(fileId: '42dd2363-51ed-4972-a382-f25ddbe11b3a', targetLocation: 'hookdir/D21-nightly-buster')]){
+							buildDebPkg_fn( ARCH, DISTRO )
 						}
-						axis {
-							name 'ARCH'
-							values arches
-						}
-					} // axes
-
-					stages{
-						stage('Clean'){
-							steps{
-								cleanWs()
-							}
-						}
-						stage('Checkout'){
-							steps{
-								fileOperations([folderCreateOperation('source')])
-								dir('source'){
-									checkout scm
-								}
-							}
-						}
-						stage("Build-${ARCH}-${DISTRO}"){
-							steps{
-								if( DISTRO == "jessie" ){
-								}else if( DISTRO == "stretch" ){
-								}else if( DISTRO == "buster" ){
-									configFileProvider([configFile(fileId: '42dd2363-51ed-4972-a382-f25ddbe11b3a', targetLocation: 'hookdir/D21-nightly-buster')]){
-										buildDebPkg_fn( ARCH, DISTRO )
-									}
-								}
-							}
-						}
-					} //stages
-				} //matrix
+					}
+				}
 			} //stage
+
 		} //stages
 	} //pipeline
 }
