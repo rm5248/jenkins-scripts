aa8eee5161892a18328a476cb071186580290d0e
Robert Middleton <robert.middleton@rm5248.com>
2020-06-19 20:38:27 -0400

    remove debug code

--- vars/buildDebPkg.groovy
+++ vars/buildDebPkg.groovy
@@ -11,7 +11,6 @@ def call( String arch, String distro, String repoHook = "" ){
 				}
 		}
 		stage("Build-${arch}-${distro}"){
-echo sh(returnStdout: true, script: 'env')
 			if( repoHook.length() > 0 ){
 				configFileProvider([configFile(fileId: ${repoHook}, targetLocation: 'hookdir/D21-repo-hook')]){
 					buildDebPkg_fn( arch, distro )
