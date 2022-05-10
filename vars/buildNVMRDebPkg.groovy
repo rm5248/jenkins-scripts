5fe3130a2fcee36bbe3b6de7dc568cf08d9d8060
Robert Middleton <rm5248@users.noreply.github.com>
2022-05-09 22:56:15 -0400

    try only arm64 pin

--- vars/buildNVMRDebPkg.groovy
+++ vars/buildNVMRDebPkg.groovy
@@ -162,8 +162,8 @@ void addBackports(String distro){
 	File f = new File("${WORKSPACE}/hookdir/D20-backports");
 	f.write "";
 	f.append "echo \"deb http://deb.debian.org/debian buster-backports main\" > /etc/apt/sources.list.d/buster-backports.list\n"
-	f.append "echo \"Package: cmake\" > /etc/apt/preferences.d/99debian-backports\n"
-        f.append "echo \"Pin: release a=buster-backports,b=*\" >> /etc/apt/preferences.d/99debian-backports\n"
+	f.append "echo \"Package: cmake:arm64\" > /etc/apt/preferences.d/99debian-backports\n"
+        f.append "echo \"Pin: release a=buster-backports\" >> /etc/apt/preferences.d/99debian-backports\n"
         f.append "echo \"Pin-Priority: 900\" >> /etc/apt/preferences.d/99debian-backports\n"
 	f.append "echo \"\" >> /etc/apt/preferences.d/99debian-backports\n"
 }
